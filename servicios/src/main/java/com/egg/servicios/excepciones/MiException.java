5f0fe8e3315126a3f40947584c9f738b61d98c6d
Martin Selis <98183961+martinselis1978@users.noreply.github.com>
2023-11-22 18:57:52 -0300

    martin 22/11

---
+++ servicios/src/main/java/com/egg/servicios/excepciones/MiException.java
@@ -0,0 +1,9 @@
+package com.egg.servicios.excepciones;
+
+public class MiException extends Exception{
+    //se genera esta clase para aislar las exepciones propias del negocio a los generales
+    public MiException(String msg) {
+        super(msg);
+    } 
+    
+}
---
+++ servicios/src/main/java/com/egg/servicios/repositorios/CategoriaRepositorio.java
@@ -0,0 +1,21 @@
+package com.egg.servicios.repositorios;
+
+import com.egg.servicios.entidades.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepositorio extends JpaRepository<Categoria, String> {
+    
+    @Query
+    ("SELECT c FROM Categoria c WHERE c.nombre = :nombre")
+    public List<Categoria> listarCategoriasNombre(@Param("nombre") String nombre);
+
+    @Query
+    ("SELECT c FROM Categoria c WHERE c.alta = true")
+    public List<Categoria> listarCategoriasAlta();
+    
+}
