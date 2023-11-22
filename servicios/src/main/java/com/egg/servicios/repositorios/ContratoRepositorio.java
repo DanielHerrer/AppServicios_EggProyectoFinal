aecb5e8a8c0849e441269e324d5f3d8fc5b6ca98
joaquin fernandez <joaquinfernandez678@gmail.com>
2023-11-22 17:05:31 -0300

    primer commit contrato

---
+++ servicios/src/main/java/com/egg/servicios/repositorios/ContratoRepositorio.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.egg.servicios.repositorios;
+
+import com.egg.servicios.entidades.Contrato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author joaquin
+ */
+@Repository
+public interface ContratoRepositorio extends JpaRepository<Contrato,String> {
+    
+}
