bce840aca1bcf05dc075d1633ff540ff4fa4475e
joaquin fernandez <joaquinfernandez678@gmail.com>
2023-11-23 20:49:20 -0300

    cuerto commit

---
+++ servicios/src/main/java/com/egg/servicios/entidades/Oferta.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.egg.servicios.entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+
+/**
+ *
+ * @author joaquin
+ */
+@Entity
+public class Oferta {
+ 
+}
