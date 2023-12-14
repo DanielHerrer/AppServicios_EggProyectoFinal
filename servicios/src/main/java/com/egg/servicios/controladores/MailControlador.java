1acab0826c8d6a59dd064e117dd01033293c5123
Martin Selis <98183961+martinselis1978@users.noreply.github.com>
2023-12-14 11:36:16 -0300

    seteo de clave

--- servicios/src/main/java/com/egg/servicios/controladores/MailControlador.java
+++ servicios/src/main/java/com/egg/servicios/controladores/MailControlador.java
@@ -2,6 +2,7 @@ package com.egg.servicios.controladores;
 
 import com.egg.servicios.servicios.MailServicio;
 import com.egg.servicios.entidades.RecuperarPassword;
+import com.egg.servicios.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,14 +19,20 @@ public class MailControlador {
     
     @Autowired
     private MailServicio mailServicio;
+    
+    @Autowired
+    private UsuarioServicio usuarioServicio;
 
     @PostMapping("/enviado")
     public ResponseEntity<Object> autenticar(@RequestBody RecuperarPassword recuperarPassword){
         
         String valor = mailServicio.generarNumeroAleatorio();
         
-        recuperarPassword.setCode(valor);
+        String mail = recuperarPassword.getEmailUser();
         
+        usuarioServicio.actualizarPassword(mail, valor);
+        
+        recuperarPassword.setCode(valor);        
         mailServicio.sendMessageUser(recuperarPassword.getEmailUser(), recuperarPassword.getCode());
         
         return ResponseEntity.ok().body("Mensaje enviado con éxito");
