9f93fe35908046232c481422a18c0e9cde7584b1
Herrera Daniel <danielherrera2009@hotmail.es>
2024-01-09 20:00:52 -0300

    modificando app properties y seguridad web para deploy render

--- servicios/src/main/java/com/egg/servicios/SeguridadWeb.java
+++ servicios/src/main/java/com/egg/servicios/SeguridadWeb.java
@@ -3,6 +3,7 @@ package com.egg.servicios;
 
 import com.egg.servicios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  *
@@ -24,9 +26,13 @@ public class SeguridadWeb extends WebSecurityConfigurerAdapter {
     public UsuarioService usuarioService;
 
     @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-        auth.userDetailsService(usuarioService)
-                .passwordEncoder(new BCryptPasswordEncoder());
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
