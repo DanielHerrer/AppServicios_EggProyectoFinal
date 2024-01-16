ef7d8af2ebd36132757e1b056276de7ab4eda1d7
Herrera Daniel <danielherrera2009@hotmail.es>
2024-01-16 11:04:30 -0300

    fix seguridad web, loop redireccion

--- servicios/src/main/java/com/egg/servicios/SeguridadWeb.java
+++ servicios/src/main/java/com/egg/servicios/SeguridadWeb.java
@@ -3,6 +3,7 @@ package com.egg.servicios;
 
 import com.egg.servicios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,6 +11,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -45,10 +49,22 @@ public class SeguridadWeb extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and().logout()
                 .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
+                .logoutSuccessHandler(logoutSuccessHandler()) // Configuramos un manejador personalizado
                 .permitAll()
-                .and().csrf()
-                .disable();
-    }  
+                .and().csrf().disable();
+    }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        return (httpServletRequest, httpServletResponse, authentication) -> {
+            // Limpiamos la sesión al hacer logout
+            HttpSession session = httpServletRequest.getSession(false);
+            if (session != null) {
+                session.removeAttribute("usuarioSession");
+            }
+            // Redirigimos a la página de login
+            httpServletResponse.sendRedirect("/login?logout");
+        };
+    }
 }
 
