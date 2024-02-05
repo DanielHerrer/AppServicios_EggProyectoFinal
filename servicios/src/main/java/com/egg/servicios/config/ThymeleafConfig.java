7efae2dc1a92bca47b60afd5b99dfa6997501fb0
Herrera Daniel <danielherrera2009@hotmail.es>
2024-02-05 11:27:50 -0300

    añadiendo formateo de fecha para HTML

--- servicios/src/main/java/com/egg/servicios/config/ThymeleafConfig.java
+++ servicios/src/main/java/com/egg/servicios/config/ThymeleafConfig.java
@@ -2,6 +2,9 @@ package com.egg.servicios.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
@@ -9,6 +12,7 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 @Configuration
 public class ThymeleafConfig implements WebMvcConfigurer {
 
+    // Configuración del motor de plantillas Thymeleaf, incluyendo el solucionador de plantillas y otras opciones personalizadas
     @Bean
     public SpringTemplateEngine templateEngine() {
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
@@ -17,6 +21,7 @@ public class ThymeleafConfig implements WebMvcConfigurer {
         return templateEngine;
     }
 
+    // Configuración del solucionador de plantillas Thymeleaf para resolver plantillas HTML en el classpath
     @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver() {
         SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
@@ -25,5 +30,15 @@ public class ThymeleafConfig implements WebMvcConfigurer {
         templateResolver.setTemplateMode("HTML");
         // Agregar más configuraciones según sea necesario
         return templateResolver;
+    }
+
+    // Configuración para formateo de fecha en las plantillas HTML
+    @Bean
+    public FormattingConversionService formattingConversionService() {
+        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setUseIsoFormat(true); // Puedes personalizar esto según tu necesidad
+        registrar.registerFormatters(conversionService);
+        return conversionService;
     }
 }
